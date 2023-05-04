ad598a68156a262f062201984f9ef691a3c9cac3
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-05-04 11:55:28 +0530

    Add project files.

---
+++ ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Design_Pattern
+{
+    // This is the Base Component that defines operations that can be altered by decorators.
+    public interface ICar
+    {
+        ICar ManufactureCar();
+    }
+}
---
+++ PetrolCarDecorator.cs
@@ -0,0 +1,23 @@
+﻿namespace Decorator_Design_Pattern
+{
+    class PetrolCarDecorator : CarDecorator
+    {
+        public PetrolCarDecorator(ICar car) : base(car)
+        {
+        }
+        public override ICar ManufactureCar()
+        {
+            car.ManufactureCar();
+            AddEngine(car);
+            return car;
+        }
+        public void AddEngine(ICar car)
+        {
+            if (car is BMWCar BMWCar)
+            {
+                BMWCar.Engine = "Petrol Engine";
+                Console.WriteLine("PetrolCarDecorator added Petrol Engine to the Car : " + car);
+            }
+        }
+    }
+}
